ad301b10893cb60260296d1e6e2d69fe9ff00b6c
kabir <kabir@kabirs-MBP.lan>
2020-01-04 02:19:16 -0800

    .

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ for epoch in range(EPOCHS):
             X, labels = batch_samples[ix], batch_labels[ix]
             predictions = clf(X)
             probas = predictions / predictions.sum()
-            sparse_output_loss += -1*probas * torch.log(probas) # entropy
+            sparse_output_loss += -1*(probas * torch.log(probas)).sum() # entropy
             report_loss += model.TotalLogLoss(predictions, labels)
 
         composed_loss = sparse_output_loss + report_loss
