6a5431d89dd304dc195c313a7fd2ea08b1499de8
kabir <kabir@kabirs-MBP.lan>
2020-01-16 13:55:17 -0800

    lanczos resampling

--- loader.py
+++ loader.py
@@ -12,7 +12,7 @@ from typing import Optional, List
 import functools
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True  # I have rare truncated data that I'm just... allowing. Don't judge me
-MAX_CACHE_SIZE = int(os.environ.get("MAX_CACHE_SIZE", 2048))
+MAX_CACHE_SIZE = int(os.environ.get("MAX_CACHE_SIZE", 4096))
 RESIZE_TARGET = (384, 512)  # anything 3:4 ratio should keep original aspect (1536, 2048) and not distort too much
 MEANS_NORMALIZE = [0.485, 0.456, 0.406]
 STDS_NORMALIZE = std = [0.229, 0.224, 0.225]
